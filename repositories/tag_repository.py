414a7c0b2d9a8bfd34e3dd08a812f84a521391b2
Jeremy Yoo <jyyoo824@gmail.com>
2021-08-14 23:25:43 +0100

    create tag_repo delete fnc

--- repositories/tag_repository.py
+++ repositories/tag_repository.py
@@ -29,7 +29,9 @@ def delete_all():
     run_sql(sql)
 
 def delete(id):
-    pass
-
+    sql = "DELETE FROM tags WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
+    
 def update(tag):
     pass
