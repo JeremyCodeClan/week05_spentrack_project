83561aab429b8b003361d9599708fe8a46c66eaf
Jeremy Yoo <jyyoo824@gmail.com>
2021-08-15 00:40:49 +0100

    create transaction_repo save fnc

--- console.py
+++ console.py
@@ -2,11 +2,14 @@ import pdb
 
 from models.merchant import Merchant
 from models.tag import Tag
+from models.transaction import Transaction
 import repositories.merchant_repository as merchant_repo
 import repositories.tag_repository as tag_repo
+import repositories.transaction_repository as transaction_repo
 
 merchant_repo.delete_all()
 tag_repo.delete_all()
+# transaction_repo.delete_all()
 
 merchant_1 = Merchant("Tesco")
 merchant_repo.save(merchant_1)
@@ -17,7 +20,13 @@ merchant_repo.save(merchant_2)
 tag_1 = Tag("Transportation")
 tag_repo.save(tag_1)
 
-tag_2 = Merchant("Entertainment")
+tag_2 = Merchant("Food")
 tag_repo.save(tag_2)
+
+transaction_1 = Transaction("Tesco food", "I got food from Tesco", 20, merchant_1, tag_2)
+transaction_repo.save(transaction_1)
+
+transaction_2 = Transaction("some transaction", "spent on something I don't know", 40)
+transaction_repo.save(transaction_2)
 
 pdb.set_trace()
---
+++ repositories/transaction_repository.py
@@ -0,0 +1,30 @@
+from db.run_sql import run_sql
+from models.transaction import Transaction
+import repositories.merchant_repository as merchant_repo
+import repositories.tag_repository as tag_repo
+
+def save(transaction):
+    sql = "INSERT INTO transactions (name, description, amount, tag_id, merchant_id) VALUES (%s, %s, %s, %s, %s) RETURNING *"
+    tag_id = None
+    merchant_id = None
+    if transaction.tag != None: tag_id = transaction.tag.id
+    if transaction.merchant != None: merchant_id = transaction.merchant.id    
+    values = [transaction.name, transaction.description, transaction.amount, tag_id, merchant_id]
+    result = run_sql(sql, values)
+    id = result[0]['id']
+    transaction.id = id
+
+def select_all():
+    pass
+
+def select(id):
+    pass
+
+def delete_all():
+    pass
+
+def delete(id):
+    pass
+
+def update(tag):
+    pass
