98be9b5f6467d9f677ccaf910614d50b98d47a55
Jeremy Yoo <jyyoo824@gmail.com>
2021-08-14 23:11:05 +0100

    create tag_repo save fnc

--- console.py
+++ console.py
@@ -1,7 +1,9 @@
 import pdb
 
 from models.merchant import Merchant
+from models.tag import Tag
 import repositories.merchant_repository as merchant_repo
+import repositories.tag_repository as tag_repo
 
 merchant_repo.delete_all()
 
@@ -10,5 +12,11 @@ merchant_repo.save(merchant_1)
 
 merchant_2 = Merchant("Asda")
 merchant_repo.save(merchant_2)
+
+tag_1 = Tag("Transportation")
+tag_repo.save(tag_1)
+
+tag_2 = Merchant("Entertainment")
+tag_repo.save(tag_2)
 
 pdb.set_trace()
--- repositories/tag_repository.py
+++ repositories/tag_repository.py
@@ -1,8 +1,12 @@
 from db.run_sql import run_sql
 from models.tag import Tag
 
-def save():
-    pass
+def save(tag):
+    sql = "INSERT INTO tags (name) VALUES (%s) RETURNING *"
+    values = [tag.name]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    tag.id = id
 
 def select_all():
     pass
