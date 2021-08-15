69d6fbacb7dc7ec7a3845e54fb5cbded84ca6566
Jeremy Yoo <jyyoo824@gmail.com>
2021-08-15 21:31:52 +0100

    create merchants_controller

---
+++ controllers/merchants_controller.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, Flask, render_template, request, redirect
+
+from models.merchant import Merchant
+import repositories.transaction_repository as transaction_repo
+import repositories.merchant_repository as merchant_repo
+import repositories.tag_repository as tag_repo
+
+merchants_blueprint = Blueprint("merchants", __name__)
+
+@merchants_blueprint.route("/merchants")
+def merchants():
+    merchants = merchant_repo.select_all()
+    return render_template("merchants/index.html", merchants = merchants)
