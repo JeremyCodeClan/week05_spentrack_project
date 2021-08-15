c46f5e354b2f921210673459215b1429ce7647b7
Jeremy Yoo <jyyoo824@gmail.com>
2021-08-15 21:32:00 +0100

    create merchants_controller

---
+++ controllers/tags_controller.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, Flask, render_template, request, redirect
+
+from models.tag import Tag
+import repositories.transaction_repository as transaction_repo
+import repositories.merchant_repository as merchant_repo
+import repositories.tag_repository as tag_repo
+
+tags_blueprint = Blueprint("tags", __name__)
+
+@tags_blueprint.route("/tags")
+def tags():
+    tags = tag_repo.select_all()
+    return render_template("tags/index.html", tags = tags)
