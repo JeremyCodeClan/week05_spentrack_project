58e1d955dbfc592a385dacb1bd4f69b6502fbec3
Jeremy Yoo <jyyoo824@gmail.com>
2021-08-17 09:48:35 +0100

    edit tag repo according to count property

--- repositories/tag_repository.py
+++ repositories/tag_repository.py
@@ -2,18 +2,18 @@ from db.run_sql import run_sql
 from models.tag import Tag
 
 def save(tag):
-    sql = "INSERT INTO tags (name) VALUES (%s) RETURNING *"
-    values = [tag.name]
+    sql = "INSERT INTO tags (name, count) VALUES (%s, %s) RETURNING *"
+    values = [tag.name, 0]
     results = run_sql(sql, values)
     id = results[0]['id']
     tag.id = id
 
 def select_all():
     tags = []
-    sql = "SELECT * FROM tags"
+    sql = "SELECT * FROM tags ORDER BY count DESC"
     results = run_sql(sql)
     for row in results:
-        tag = Tag(row['name'], row['id'])
+        tag = Tag(row['name'], row['count'], row['id'])
         tags.append(tag)
     return tags
 
@@ -23,7 +23,7 @@ def select(id):
     sql = "SELECT * FROM tags WHERE id = %s"
     values = [id]
     result = run_sql(sql, values)[0]
-    tag = Tag(result['name'], result['id'])
+    tag = Tag(result['name'], result['count'], result['id'])
     return tag
 
 def delete_all():
@@ -36,6 +36,6 @@ def delete(id):
     run_sql(sql, values)
 
 def update(tag):
-    sql = "UPDATE tags SET name = %s  WHERE id = %s"
-    values = [tag.name, tag.id]
+    sql = "UPDATE tags SET (name, count) = (%s, %s)  WHERE id = %s"
+    values = [tag.name, tag.count, tag.id]
     run_sql(sql, values)
