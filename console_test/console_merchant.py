ab70d6ee4254fec674fdb1ff7cd0ef47c5594423
Jeremy Yoo <jyyoo824@gmail.com>
2021-08-18 21:15:04 +0100

    edit dummy date merchant file

--- console_test/console_merchant.py
+++ console_test/console_merchant.py
@@ -1,7 +1,33 @@
 from models.merchant import Merchant
 
-merchant_4 = Merchant("Electricity")
-merchant_3 = Merchant("Udemy")
-merchant_2 = Merchant("Blizzard")
-merchant_1 = Merchant("Tesco")
-merchants = [merchant_4, merchant_3, merchant_2, merchant_1]
+merchant_unicef = Merchant("Unicef")
+merchant_bus = Merchant("City Bus")
+merchant_udemy = Merchant("Udemy")
+merchant_immigration = Merchant("Immigration")
+merchant_water = Merchant("Water")
+merchant_insurance = Merchant("Insurance")
+merchant_electricity = Merchant("Electricity")
+merchant_cineworld = Merchant("Cineworld")
+merchant_gym = Merchant("The Gym")
+merchant_zoo = Merchant("Edinburgh Zoo")
+merchant_steam = Merchant("Steam")
+merchant_blizzard = Merchant("Blizzard")
+merchant_sainsbury = Merchant("Sainsbury's")
+merchant_tesco = Merchant("Tesco")
+merchant_lidl = Merchant("Lidl")
+merchant_asda = Merchant("Asda")
+merchant_aldi = Merchant("Aldi")
+merchant_asos = Merchant("Asos")
+merchant_samsung = Merchant("Samsung")
+merchant_apple = Merchant("Apple")
+
+merchants = [
+    merchant_bus, merchant_unicef,
+    merchant_apple, merchant_samsung, merchant_asos, merchant_aldi, merchant_lidl, merchant_immigration,
+    merchant_tesco, merchant_sainsbury, merchant_blizzard, merchant_steam, merchant_zoo,
+    merchant_gym, merchant_cineworld, merchant_electricity, merchant_insurance, merchant_water, merchant_udemy
+    ]
+
+
+
+
