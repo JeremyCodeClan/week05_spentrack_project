eb461af2ea23bb10e834a348d75c04d341fc2840
Jeremy Yoo <jyyoo824@gmail.com>
2021-08-18 21:15:14 +0100

    edit dummy date tag file

--- console_test/console_tag.py
+++ console_test/console_tag.py
@@ -1,7 +1,23 @@
 from models.tag import Tag
 
-tag_4 = Tag("Utility")
-tag_3 = Tag("Education")
-tag_2 = Tag("Entertainment")
-tag_1 = Tag("Food")
-tags = [tag_4, tag_3, tag_2, tag_1]
+tag_morewhatever = Tag("More whatever")
+tag_whatever = Tag("Whatever")
+tag_education = Tag("Education")
+tag_expenses = Tag("Expenses")
+tag_bills = Tag("Bills")
+tag_charity = Tag("Charity")
+tag_eating = Tag("Eating Out")
+tag_entertainment = Tag("Entertainment")
+tag_family = Tag("Family")
+tag_finances = Tag("Finances")
+tag_gifts = Tag("Gifts")
+tag_groceries = Tag("Groceries")
+tag_general = Tag("General")
+tag_shopping = Tag("Shopping")
+tag_transport = Tag("Transportation")
+
+tags = [
+    tag_morewhatever, tag_whatever, tag_education,
+    tag_transport, tag_shopping, tag_general, tag_groceries, tag_gifts, tag_expenses,
+    tag_finances, tag_family, tag_entertainment, tag_eating, tag_charity, tag_bills
+]
